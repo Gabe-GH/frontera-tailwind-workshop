b9054d54d4e238cf52eff997a02dbac8b1b39d9f
Gabe-GH <gabriel.lira01@utrgv.edu>
2025-04-16 16:50:20 -0500

    workshop code

---
+++ src/Part-3/pages/Home.tsx
@@ -0,0 +1,5 @@
+// pages/Home.jsx
+export default function Home() {
+    return <h1>Welcome to the Home page!</h1>;
+  }
+  
---
+++ src/Part-3/pages/NotFound.tsx
@@ -0,0 +1,5 @@
+// pages/NotFound.jsx
+export default function NotFound() {
+    return <h1>404 - Page Not Found 😵</h1>;
+  }
+  
